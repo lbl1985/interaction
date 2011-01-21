5b8e4748bcf04184269fb9e58788e860e9b1b5b4
lbl1958_PC <herbert19lee@gmail.com>
2011-01-21 11:20:28 -0500

    tracking with skin detection

--- tvinteration/skinDetection/skinDetectionTrial1.m
+++ tvinteration/skinDetection/skinDetectionTrial1.m
@@ -1,6 +1,7 @@
 % vcm
-Type = 2; Clip = 6;
-record = 0; moviefile = 'trial2.avi';
+Type = 2; Clip = 4;
+record = 0; 
+moviefile = 'trial1.avi'; moviefile2 = 'trial2.avi';
 numRegions = 10;
 % [srcdirI filenamesI] = rfdatabase(datadir(Type, 'kthvideo'), 'person', '.avi');
 [srcdirI filenamesI] = rfdatabase(datadir_interaction(Type, 'tvinteraction'), [], '.avi');
@@ -12,7 +13,6 @@ mat = movie2var(filename, 0, 1);
 
 % nframe = size(mat, ndims(mat));
 
-
 info = readTVdatasetAnnotation(filenameA);
 info.Type = Type;
 info.Clip = Clip;
@@ -22,6 +22,7 @@ skinMat = zeros(size(mat, 1), size(mat, 2), nframe);
 if record
     aviobj = avifile(moviefile, 'fps', 22, 'compression', 'none');
 end
+
 skinColor = zeros(size(mat));
 skinRegions = cell(nframe, numRegions);
 for i = 1 : nframe
@@ -73,4 +74,44 @@ if record
     aviobj = close(aviobj);
 end
 
+diffMatrix = skinRegionsTracking(mat, skinRegions);
+save(['diffMatrixType' num2str(Type) 'Clip' num2str(Clip)], 'diffMatrix',  'skinRegions');
+trajectory = RegionTrajectory(diffMatrix);
 
+% load diffMatrixType2Clip4.mat
+
+if record
+    aviobj = avifile('trial3.avi', 'fps', 22, 'compression', 'none');
+end
+
+for i = 1 : size(trajectory, 2)
+    im = mat(:, :, :, i);
+    h = figure(1); subplot(2, 3, 1); 
+    imshow(im);
+    title(['Frame ' num2str(i)]);
+    
+    im = double(im);
+    skinprob = computeSkinProbability(im);
+    skin = (skinprob >0) + 0;
+    skinMat(:, :, i) = skin;
+    skinColor(:, :, :, i) = repmat(skin, [1 1 3]); 
+    figure(h); h2 = subplot(2, 3, 4); 
+    ideaShow(info, 'tvAnnotation_data', skinMat, i, h2);
+    
+    for j = 1 : 4
+        skinShow = zeros(size(skin));
+        id = trajectory(j, i);
+        skinShow(skinRegions{i, id}) = 1;
+        skinShowTemp = uint8(im .* repmat(skinShow, [1 1 3]));
+%         figure(h); subplot(2, 3, j + floor(j/3) + 1); imshow(skinRegions{i, j});        
+        figure(h); subplot(2, 3, j + floor(j/3) + 1); imshow(skinShowTemp);
+    end
+    
+    if record
+        frame = getframe(gcf);
+        aviobj = addframe(aviobj, frame);
+    end
+end
+if record
+    aviobj = close(aviobj);
+end
--- tvinteration/skinDetection/skinRegionsTracking.m
+++ tvinteration/skinDetection/skinRegionsTracking.m
@@ -1,18 +1,28 @@
 % function diffMatrix = skinRegionsTracking(skinRegions)
-function diffMatrix = skinRegionsTracking(im, skinRegions)
+function diffMatrix = skinRegionsTracking(mat, skinRegions)
 [nframe numRegions]= size(skinRegions);
-diffMatrix = zeros(numRegions, numRegions, nframe);
+diffMatrix = zeros(numRegions, numRegions, nframe-1);
+siz = size(mat);
+
 % matlabpool open
-for i = 1 : nframe
+for i = 1 : nframe-1
     diffM = zeros(numRegions, numRegions);   
+    currFrame = mat(:, :, :, i);
+    nextFrame = mat(:, :, :, i + 1);
+%     cskinRegion = skinRegions(i, :);
+%     nskinRegion = skinRegions(i + 1, :);
+    
     for j = 1 : numRegions
-        for k = j : numRegions
-            if k == j
-                diffM(j, k) = inf;
-            else
-                diffM(j, k) = norm3d(skinRegions{i, j}, skinRegions{i, k});
-                diffM(k, j) = diffM(j, k);
-            end
+        for k = 1 : numRegions
+            currMask = zeros(siz(1:2));
+            nextMask  = currMask;    
+            currMask(skinRegions{i, j}) = 1;
+            nextMask(skinRegions{i + 1, k}) = 1;
+            
+            cskinRegionReal = double(currFrame) .* repmat(currMask, [1 1 3]);
+            nskinRegionReal = double(nextFrame) .* repmat(nextMask, [1 1 3]);
+            diffM(j, k) = norm3d(cskinRegionReal, nskinRegionReal);
+            diffM(k, j) = diffM(j, k);
         end
     end
     diffMatrix(:, :, i) = diffM;
