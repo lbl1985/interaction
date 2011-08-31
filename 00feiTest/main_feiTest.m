082df1a248ec7b38a539a7c1b4b692d4978b378d
Binlong Li <herbert19lee@gmail.com>
2011-08-31 15:02:42 -0400

    feiTest main file

--- 00feiTest/main_feiTest.m
+++ 00feiTest/main_feiTest.m
@@ -1,5 +1,19 @@
+clc
 baseFolder = getProjectBaseFolder();
 [srcdir filenames n] = rfdatabase(fullfile(getProjectBaseFolder(), ...
     '00feiTest', 'testData'), [], '.mat');
+HankelWindowSize = 7;
+pca_dim = 4;
 
-% for i = 1 : n
+for i = 1: n
+    I = load(fullfile(srcdir, filenames{i}));
+    I = I.I;
+    nVideo = length(I);
+    HankelMatrixBatch = cell(nVideo, 1);
+    for j = 1 : nVideo
+        HankelMatrixBatch{j} = hankelConstruction(I{j}', HankelWindowSize);
+    end
+    groupsLabel = [zeros(nVideo/2, 1); ones(nVideo/2, 1)];
+    [CorrectRate CPMatrix trainBatch] = Classifier_interaction(HankelMatrixBatch, groupsLabel, 'OneHankelAsOneSet_SVM_pcadimSVM_Batch', 1, pca_dim, 'poly', 2);
+    display([ filenames{i}(1:end-4) 'AR: ' num2str(CorrectRate)]); 
+end
